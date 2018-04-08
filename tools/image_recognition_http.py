baa9ccff9d1fdb41e2293427edfadeb763cdd9c9
definesun <782622509@qq.com>
2018-04-08 16:58:42 +0800

    1、图像识别服务测试版本

---
+++ tools/image_recognition_http.py
@@ -0,0 +1,33 @@
+from image_recognition import img_rec
+import json
+import base64
+import cv2
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/imageRecognition', methods=['POST'])
+def image_recog():
+    print(request.json)
+        
+    img_base64 = request.json['image']
+    img = base64.b64decode(img_base64)
+    bb=img_rec(img)
+    return json.dumps(bb)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('/tmp/tmp.jpg')
+        img = cv2.imread('/tmp/tmp.jpg')
+        bb=img_rec(img)
+
+        return json.dumps({'result': bb})
+    return 'Hello, World!'
---
+++ tools/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export FLASK_APP=image_recognition_http.py
+python -m flask run 
